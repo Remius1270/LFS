a96e2ddeaa1cd3ef6547ddfb973570c0b3d65795
Remius1270 <banchoprod@gmail.com>
2017-12-26 18:22:59 +0100

    //
    //

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ ini_set('display_errors', '1');
 
 $path = "/var/www/html";
 
-	include_once($path."/html/controller/Controller.php");
+	include_once($path."/controller/Controller.php");
 
 	$controller = new Controller();
 	$controller->invoke();
